aef7de6d6ac80fae2d984eeeee7adf275d6b8438
Manal123 <manal05116@gmail.com>
2026-01-22 01:22:28 +0800

    Add build files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/online/'
+   base: '/online/',
+    build: {
+    outDir: 'docs'  // dist-ийн оронд docs
+  }
 })
